3b24c6b40f44a9505073f63e456f80b5cd2858df
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-28 14:09:07 +0700

    menambahkan isi file ContainerApp.kt

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -1,2 +1,30 @@
 package com.example.pertemuan9.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.pertemuan9.room.DatabaseSiswa
+import com.example.pertemuan9.repositori.RepositoriSiswa
+import com.example.pertemuan9.repositori.OfflineRepositoriSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).SiswaDao()
+        )
+
+    }
+}
+
+class AplikasiSiswa : Application() {
+
+    lateinit var container: ContainerApp
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
