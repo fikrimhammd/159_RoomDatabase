3af0371f28aa816541cca94e330812e49d58a0cb
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-28 14:07:15 +0700

    menambahkan isi di file DatabaseSiswa.kt

--- app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/DatabaseSiswa.kt
@@ -1,2 +1,29 @@
 package com.example.pertemuan9.room
 
+import android.content.Context
+import androidx.room.Database // Import yang benar
+import androidx.room.Room // Import yang benar
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+
+    abstract fun SiswaDao(): SiswaDao
+
+    companion object {
+        @Volatile
+        private var instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    DatabaseSiswa::class.java,
+                    "siswa_database"
+                )
+                    .build()
+                    .also { instance = it }
+            }
+        }
+    }
+}
