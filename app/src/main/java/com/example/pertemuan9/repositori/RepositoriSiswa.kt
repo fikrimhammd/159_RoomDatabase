e1df7ae07c612b433793a0b7e75832a0e1f33974
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-28 14:08:25 +0700

    menambahkan isi file RepositoriSiswa.kt

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -1,2 +1,20 @@
 package com.example.pertemuan9.repositori
 
+import com.example.pertemuan9.room.Siswa
+import com.example.pertemuan9.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    // 1. DIPERBAIKI: Panggil fungsi dengan ()
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+
+    // 2. DIPERBAIKI: Gunakan instance 'siswa' yang diterima sebagai parameter
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
